874e3daafa22010d683bd9eafe4fc806c96b19ac
Ilnur <ilnrzakirov@gmail.com>
2022-10-26 19:05:19 +0300

    Добавил аннотации типов

--- repositories/users.py
+++ repositories/users.py
@@ -1,19 +1,20 @@
+from models.Users import User
 from repositories.base import BaseRepository
 
 
 class UserRepository(BaseRepository):
 
-    async def get_all(self, limit: int = 10, skip: int = 0):
+    async def get_all(self, limit: int = 10, skip: int = 0) -> list[User]:
         pass
 
-    async def get_by_id(self, id: int):
+    async def get_by_id(self, id: int) -> User:
         pass
 
-    async def create(self):
+    async def create(self) -> User:
         pass
 
-    async def get_by_email(self, email: str):
+    async def get_by_email(self, email: str) -> User:
         pass
 
-    async def update(self):
+    async def update(self) -> User:
         pass
