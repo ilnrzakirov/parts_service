9f5bdbbd88d4effb1e1d5c5ababcd5f3d8c7b172
bcarlee <ilnrzakirov@gmail.om>
2022-11-04 18:42:24 +0300

    Добавил энпоинт для регистрации юзера

--- endpoint/user_endpoints.py
+++ endpoint/user_endpoints.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
 
 from models.Users import UserIn
+from repositories.users import UserRepository
 
 user_router = APIRouter()
 
 
 @user_router.post("/create/")
 async def create_user(user_in: UserIn):
-    print(user_in)
+    user = UserRepository()
+    try:
+        await user.create(user_in)
+        return {"msg": "Ok"}
+    except Exception as error:
+        return {"msg": "Юзер уже существует"}
